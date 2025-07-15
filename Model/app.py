24033fc17937c2b0845a7d36894bdf6fe5912e09
MuhammadUmarAbdulAziz <abdulazizumar01@gmail.com>
2025-07-15 08:05:59 +0700

    Update app.py

--- Model/app.py
+++ Model/app.py
@@ -27,7 +27,7 @@ st.markdown("""
 # --- LOAD MODEL ---
 @st.cache_resource
 def load_model():
-    return joblib.load("best_xgb_model.pkl")
+    return joblib.load("Model/best_xgb_model.pkl")
 
 model = load_model()
 
